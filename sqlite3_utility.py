0d55c7aaf33bb5fda8c2b77d190e09b904ef2ed8
Tech Tana <13774129+techtana@users.noreply.github.com>
2021-05-05 17:37:49 -0600

    Update sqlite3_utility.py

--- sqlite3_utility.py
+++ sqlite3_utility.py
@@ -4,16 +4,18 @@ Utility for using sqlite3 built on top of sqlite3
 import sqlite3
 import contextlib
 import json
-
+import pandas
+import pickle
 
 def replace_specialchars2underscore(string):
     import re
-    return re.sub("'|-| ", "_", str(string))
+    string = re.sub("-| ", "_", str(string))
+#     string = string.encode('latin1')
+    return string
 
 def replace_single2doublequote(string):
     import re
-    return re.sub(r"\'|'", "", str(string))
-
+    return re.sub(r"\'|'", "\"", str(string))
 
 def sqlite3_safe_execute(db_path:str, statement:str, fetch:bool=False):
     """
@@ -28,10 +30,6 @@ def sqlite3_safe_execute(db_path:str, statement:str, fetch:bool=False):
                 cur = cursor.execute(statement)
                 if fetch==True:
                     return cur.fetchall()
-                else:
-                    # you cannot operate on closed cursor.
-                    # this return is not useful
-                    return cur
             
 def sqlite3_get_tableinfo(db_path:str, table:str):
     """
@@ -67,7 +65,8 @@ def sqlite3_add_record(db_path:str, table:str, column_vals:list):
     column_indices, column_names, column_type, column_notnull, column_pk = sqlite3_get_tableinfo(db_path, table)
     try:
         if not len(column_names) == len(column_vals):
-            raise ValueError(f"Unexpected length for column_vals; expect {len(column_names)}: {column_names}")    
+            raise ValueError(f"Unexpected length for column_vals; expect {len(column_names)}: {column_names}")  
+            
         column_names_str = ','.join([str(name) for name in column_names])
         column_vals_str = ','.join([str(val) for val in column_vals])
         statement = f"INSERT INTO {table} ({column_names_str}) VALUES ({column_vals_str})"
@@ -76,8 +75,31 @@ def sqlite3_add_record(db_path:str, table:str, column_vals:list):
         error_message = f"{type(e).__name__}: {str(e)}"
         print("Failed to add record to table. If Integrity error is found, "
               "this could be caused by adding a record with duplicated primary key, "
-              "or the table does not have adequate primary key structure.")
+              "or the table does not have adequate primary key structure, "
+              "or the incoming data is not valid e.g. for binary data, use `sqlite3_add_record_binary`.")
         print(error_message)
+        
+def sqlite3_add_record_binary(db_path:str, table:str, column_vals:list):
+    """
+    add new records in sqlite3 table (BINARY data)
+    """
+    column_indices, column_names, column_type, column_notnull, column_pk = sqlite3_get_tableinfo(db_path, table)
+    try:
+        if not len(column_names) == len(column_vals):
+            raise ValueError(f"Unexpected length for column_vals; expect {len(column_names)}: {column_names}")  
+            
+        column_names_str = ','.join([str(name) for name in column_names])
+        column_vals_kw = ','.join(['?' for val in column_vals])
+        column_vals_bin = tuple(sqlite3.Binary(data) for data in column_vals)
+        statement = f'''INSERT INTO {table} ({column_names_str}) VALUES ({column_vals_kw})'''
+        with contextlib.closing(sqlite3.connect(db_path)) as conn: # auto-closes
+            with conn: # auto-commits
+                with contextlib.closing(conn.cursor()) as cursor: # auto-closes
+                    cursor.execute(statement,column_vals_bin)
+    except Exception as e:
+        error_message = f"{type(e).__name__}: {str(e)}"
+        print("Failed to add record to table with sqlite3_add_record_binary.")
+        raise e
                 
 def sqlite3_update_record(db_path:str, table:str, primary_keys:list, update_vals:list):     
     """
@@ -85,8 +107,8 @@ def sqlite3_update_record(db_path:str, table:str, primary_keys:list, update_vals
     """
     column_indices, column_names, column_type, column_notnull, column_pk = sqlite3_get_tableinfo(db_path, table)
     if not len(column_pk) == len(primary_keys):
-        raise ValueError(f"Unexpected length for columns_vals; expect {len(column_names)}: {column_names}")
-    if not len(column_names) == len(primary_keys+update_vals):
+        raise ValueError(f"Unexpected length for primary_keys; expect {len(column_names)}: {column_names}")
+    if not len(column_names) == len(set(primary_keys+update_vals)):
         raise ValueError(f"Unexpected length for columns_vals; expect {len(column_names)}: {column_names}")
         
     statement = f"""
@@ -107,7 +129,7 @@ def sqlite3_parse_datatypes(dtype):
     
     ONLY SUPPORT: TEXT, NUM, INT, REAL, "" (BLOB: empty string)
     (2021-05-05) all TEXT will now become a BLOB. This is to prevent invalid quotation characters. 
-                 all string will be processed by json.dumps() to prevent this issue.
+                 all string will be processed by pickle.dumps(data, 0) to prevent this issue.
     DATA TYPE documentation at https://sqlite.org/lang_createtable.html
     """
     if not type(dtype) == type:
@@ -118,7 +140,6 @@ def sqlite3_parse_datatypes(dtype):
     elif dtype == float:
         return "REAL"
     return ""
-
 
 def sqlite3_json_to_table(db_path:str, new_table:str, json_list:list, PK_list:list):     
     """
@@ -153,14 +174,55 @@ def sqlite3_json_to_table(db_path:str, new_table:str, json_list:list, PK_list:li
         for item in json_list:
             columns_vals = [item[key] for key in column_names]
             columns_vals_zip = list(zip(columns_vals, column_types_parsed))
-            # Convert data object that is not supported by SQLITE to string `json.dumps`
-            # They can be parsed back using `json.loads` 
-            columns_vals_converted = [f"'{json.dumps(replace_single2doublequote(pair[0]))}'" if pair[1]=="" 
-#                                       else f"'{replace_single2doublequote(pair[0])}'" if pair[1]=="TEXT" 
+            # Convert data object that is not supported by SQLITE to string `pickle.dumps(data, 0)`
+            # They can be parsed back using `pickle.loads` 
+            columns_vals_converted = [f"'{pickle.dumps(pair[0], 0)}'" if pair[1]=="" 
                                       else pair[0] 
                                       for pair in columns_vals_zip]
             sqlite3_add_record(db_path, new_table, columns_vals_converted)
 
+        print("JSON data imported into SQLITE table")
+        
+    except Exception as e:
+        # drop the new table if adding record fails
+        error_message = f"{type(e).__name__}: {str(e)}"
+        statement = f""" DROP TABLE {new_table} """
+        cursor = sqlite3_safe_execute(db_path, statement)
+
+def sqlite3_pandas_to_table(db_path:str, new_table:str, pandas_df:pd.core.frame.DataFrame, PK_list:list):     
+    """
+    import Pandas DF into sqlite3 database as new table
+    TABLE's Column Name and Data Type is defined by the pandas columns
+    
+    """
+    # validate pandas_df format : list of dict
+    if not type(pandas_df) == pd.core.frame.DataFrame:
+        raise ValueError("pandas_df is not a valid pd.core.frame.DataFrame")
+    
+    column_names = list(pandas_df.columns)
+    # DEFAULT column_types to BLOB or empty string
+    column_types_parsed = ["" for dtype in column_names]
+    column_notnull = ["NOT NULL" if col in PK_list else "" for col in column_names]
+
+    # validate PK_list : must exists in JSON dict keys
+    if not all([True if col in column_names else False for col in PK_list]):
+        raise ValueError("some items in PK_list does not exist in column_names")
+    
+    # create a new table
+    column_names_parsed = [replace_specialchars2underscore(string) for string in column_names]
+    column_definition = [" ".join(line) for line in list(zip(column_names_parsed, column_types_parsed, column_notnull))]
+    statement = f""" CREATE TABLE {new_table}({",".join(column_definition)},PRIMARY KEY ({','.join(PK_list)})) """
+    print(statement)
+    cursor = sqlite3_safe_execute(db_path, statement)
+    print("New table added to database")  
+    
+    try:
+        # import JSON content to database table
+        for _index,item in pandas_df.iterrows():
+            columns_vals = [item[key] for key in column_names]
+            columns_vals_bin = [pickle.dumps(val, 0) for val in columns_vals]
+            sqlite3_add_record_binary(db_path, new_table, columns_vals_bin)
+            
         print("JSON data imported into SQLITE table")
         
     except Exception as e:
@@ -172,9 +234,9 @@ def sqlite3_json_to_table(db_path:str, new_table:str, json_list:list, PK_list:li
 
 def sqlite3_parse_table2dataframe(db_path, table):
     """
-    Convert TEXT->str, INT->int, REAL->float, ''->json.loads()
-    This is assuming that all BLOBS are text string converted by json.dumps().
-    json.loads() will convert these objects back to original data objects.
+    Convert TEXT->str, INT->int, REAL->float, ''->pickle.dumps(data, 0)
+    This is assuming that all BLOBS are text string converted by pickle.dumps().
+    pickle.loads() will convert these objects back to original data objects.
     """
     import pandas as pd
     with contextlib.closing(sqlite3.connect(db_path)) as conn: # auto-closes
@@ -187,7 +249,7 @@ def sqlite3_parse_table2dataframe(db_path, table):
     # parse data type in each column
     for item in column_definition_zip:
         if item[1]=="":
-            df[item[0]] = df[item[0]].apply(lambda x: json.loads(x))
+            df[item[0]] = df[item[0]].apply(lambda x: pickle.loads(x))
         elif item[1]=="TEXT":
             df[item[0]] = df[item[0]].apply(lambda x: str(x))
         elif item[1]=="INT":
